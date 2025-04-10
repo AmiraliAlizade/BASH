86041f5256ccf6a46365a2731c6c59789246dca6
Amirali Alizadeh <amir.ali.alizade.2009@gmail.com>
2025-04-10 18:41:16 +0330

    The list of houses is almost ready

--- src/components/houses/Houses.jsx
+++ src/components/houses/Houses.jsx
@@ -7,34 +7,114 @@ import { FaDollarSign } from "react-icons/fa";
 export default function Houses() {
   return (
     <div className="houses-list-container">
-      <ul className="houses-list">
-        <li className="houses-item">
-          <div className="house-img">
-            <img src={house} alt="" />
-          </div>
-          <div className="house-props">
-            <h1 className="house-title">A big villa in the LA</h1>
-            <div className="house-description">
-              <p>
-                A beatiful almost new and big villa in the north of LA it has 3
-                bathroom 2 at the building and one outside
-              </p>
+      <div className="">
+        <ul className="houses-list">
+          <li className="houses-item">
+            <div className="house-img">
+              <img src={house} alt="" />
             </div>
-            <div className="house-info">
-              <span className="house-price">
-                <FaDollarSign></FaDollarSign>200000
-              </span>
-              <span className="house-madein">
-                2023
-                <BsFillCalendarDateFill className="date-icon"></BsFillCalendarDateFill>
-              </span>
-              <span className="house-size">
-                995<TbMeterSquare className="meter-icon"></TbMeterSquare>
-              </span>
+            <div className="house-props">
+              <h1 className="house-title">A big villa in the LA</h1>
+              <div className="house-description">
+                <p>
+                  A beatiful almost new and big villa in the north of LA it has
+                  3 bathroom 2 at the building and one outside
+                </p>
+              </div>
+              <div className="house-info">
+                <span className="house-price">
+                  <FaDollarSign></FaDollarSign>200000
+                </span>
+                <span className="house-madein">
+                  2023
+                  <BsFillCalendarDateFill className="date-icon"></BsFillCalendarDateFill>
+                </span>
+                <span className="house-size">
+                  995<TbMeterSquare className="meter-icon"></TbMeterSquare>
+                </span>
+              </div>
             </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+          <li className="houses-item">
+            <div className="house-img">
+              <img src={house} alt="" />
+            </div>
+            <div className="house-props">
+              <h1 className="house-title">A big villa in the LA</h1>
+              <div className="house-description">
+                <p>
+                  A beatiful almost new and big villa in the north of LA it has
+                  3 bathroom 2 at the building and one outside
+                </p>
+              </div>
+              <div className="house-info">
+                <span className="house-price">
+                  <FaDollarSign></FaDollarSign>200000
+                </span>
+                <span className="house-madein">
+                  2023
+                  <BsFillCalendarDateFill className="date-icon"></BsFillCalendarDateFill>
+                </span>
+                <span className="house-size">
+                  995<TbMeterSquare className="meter-icon"></TbMeterSquare>
+                </span>
+              </div>
+            </div>
+          </li>
+          <li className="houses-item">
+            <div className="house-img">
+              <img src={house} alt="" />
+            </div>
+            <div className="house-props">
+              <h1 className="house-title">A big villa in the LA</h1>
+              <div className="house-description">
+                <p>
+                  A beatiful almost new and big villa in the north of LA it has
+                  3 bathroom 2 at the building and one outside
+                </p>
+              </div>
+              <div className="house-info">
+                <span className="house-price">
+                  <FaDollarSign></FaDollarSign>200000
+                </span>
+                <span className="house-madein">
+                  2023
+                  <BsFillCalendarDateFill className="date-icon"></BsFillCalendarDateFill>
+                </span>
+                <span className="house-size">
+                  995<TbMeterSquare className="meter-icon"></TbMeterSquare>
+                </span>
+              </div>
+            </div>
+          </li>
+          <li className="houses-item">
+            <div className="house-img">
+              <img src={house} alt="" />
+            </div>
+            <div className="house-props">
+              <h1 className="house-title">A big villa in the LA</h1>
+              <div className="house-description">
+                <p>
+                  A beatiful almost new and big villa in the north of LA it has
+                  3 bathroom 2 at the building and one outside
+                </p>
+              </div>
+              <div className="house-info">
+                <span className="house-price">
+                  <FaDollarSign></FaDollarSign>200000
+                </span>
+                <span className="house-madein">
+                  2023
+                  <BsFillCalendarDateFill className="date-icon"></BsFillCalendarDateFill>
+                </span>
+                <span className="house-size">
+                  995<TbMeterSquare className="meter-icon"></TbMeterSquare>
+                </span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
