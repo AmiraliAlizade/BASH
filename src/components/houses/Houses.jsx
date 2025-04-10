f898896496a545d8a1614ece5222515d6f21de91
Amirali Alizadeh <amir.ali.alizade.2009@gmail.com>
2025-04-10 18:18:36 +0330

    The house card almost finished

--- src/components/houses/Houses.jsx
+++ src/components/houses/Houses.jsx
@@ -1,5 +1,8 @@
 import "./Houses.css";
 import house from "../../data/bash-high-resolution-logo.png";
+import { TbMeterSquare } from "react-icons/tb";
+import { BsFillCalendarDateFill } from "react-icons/bs";
+import { FaDollarSign } from "react-icons/fa";
 
 export default function Houses() {
   return (
@@ -11,10 +14,23 @@ export default function Houses() {
           </div>
           <div className="house-props">
             <h1 className="house-title">A big villa in the LA</h1>
+            <div className="house-description">
+              <p>
+                A beatiful almost new and big villa in the north of LA it has 3
+                bathroom 2 at the building and one outside
+              </p>
+            </div>
             <div className="house-info">
-              <span className="house-price">$200000</span>
-              <span className="house-madein">2023</span>
-              <span className="house-size">995</span>
+              <span className="house-price">
+                <FaDollarSign></FaDollarSign>200000
+              </span>
+              <span className="house-madein">
+                2023
+                <BsFillCalendarDateFill className="date-icon"></BsFillCalendarDateFill>
+              </span>
+              <span className="house-size">
+                995<TbMeterSquare className="meter-icon"></TbMeterSquare>
+              </span>
             </div>
           </div>
         </li>
