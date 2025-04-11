63bcc477ce248e0225ad2bbb242f00293d5f8241
Amirali Alizadeh <amir.ali.alizade.2009@gmail.com>
2025-04-11 18:13:05 +0330

    Process of creating sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "./Sidebar.css";
 
 export default function Sidebar() {
-  return <></>;
+  return (
+    <>
+      <div className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-item">Your advertisements</li>
+          <li className="sidebar-item">Settings</li>
+          <li className="sidebar-item">Settings</li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Houses from "../components/houses/Houses";
+import Sidebar from "../components/sidebar/Sidebar";
 
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
-      <Houses></Houses>
+      <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+        <Houses></Houses>
+        <Sidebar></Sidebar>
+      </div>
     </div>
   );
 }
